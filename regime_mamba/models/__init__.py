bacc2ded6f2a968c8c77b3581d77a912411f5f76
김지원 <127197114+jiwon103203@users.noreply.github.com>
2025-03-13 17:22:55 +0900

    Update __init__.py

--- regime_mamba/models/__init__.py
+++ regime_mamba/models/__init__.py
@@ -1,3 +1,4 @@
 from .mamba_model import TimeSeriesMamba, create_model_from_config
+from .rl_model import ActorCritic
 
-__all__ = ['TimeSeriesMamba', 'create_model_from_config']
+__all__ = ['TimeSeriesMamba', 'create_model_from_config', 'ActorCritic']
