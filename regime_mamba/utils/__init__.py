00d9bb416520211671ada38468044b2d5d66d1cf
김지원 <127197114+jiwon103203@users.noreply.github.com>
2025-03-13 18:00:30 +0900

    Update __init__.py

--- regime_mamba/utils/__init__.py
+++ regime_mamba/utils/__init__.py
@@ -1,7 +1,7 @@
 
 from .utils import set_seed
 
-from .rl_agents import PPQAgent
+from .rl_agents import PPOAgent
 
 from .rl_environments import FinancialTradingEnv
 
@@ -9,4 +9,4 @@ from .rl_investment import run_rl_investment
 
 from .rl_visualize import visualize_rl_results, visualize_all_results, visualize_training_history, plot_rolling_sharpe_comparison
 
-__all__ = ['set_seed', 'PPQAgent', 'FinancialTradingEnv', 'run_rl_investment', 'visualize_rl_results', 'visualize_all_results', 'visualize_training_history', 'plot_rolling_sharpe_comparison']
+__all__ = ['set_seed', 'PPOAgent', 'FinancialTradingEnv', 'run_rl_investment', 'visualize_rl_results', 'visualize_all_results', 'visualize_training_history', 'plot_rolling_sharpe_comparison']
